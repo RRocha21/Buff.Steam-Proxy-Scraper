189dc48ebb567b186b62ef7e34ad06b5bf65ad5e
G2-Rocha <132342030+G2-Rocha@users.noreply.github.com>
2024-03-15 13:48:09 +0000

    fix

--- Steam2Buff.sh
+++ Steam2Buff.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 # Run the first .sh file
-
-
-
 cd Steam2Buff
 chmod +x start.sh
 ./start.sh
 cd ..
-
-chmod +x ../Steam2Buff-Scraper/buff_proxy_socks5_list.txt
-chmod +x ../Steam2Buff-Scraper/buff_proxy_socks4_list.txt
-chmod +x ../Steam2Buff-Scraper/buff_proxy_http_list.txt
 
 chmod +x Steam2Buff/out/proxies/steam_proxy_socks5_list.txt
 chmod +x Steam2Buff/out/proxies/steam_proxy_socks4_list.txt
@@ -25,4 +18,3 @@ cp -v Steam2Buff/out/proxies/steam_proxy_socks4_list.txt ../Steam2Buff-Scraper
 cp -v Steam2Buff/out/proxies/steam_proxy_socks5_list.txt ../Steam2Buff-Scraper
 
 echo "Batch script completed."
-read -p "Press Enter to exit."
